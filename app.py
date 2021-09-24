c917b2475e5ff566ec066fcaf42b2e998a5354d3
John <jmcarter211@gmail.com>
2021-09-23 22:03:15 -0400

    fixed service and improved the code

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 '''
 John Carter
-Flask server for Pi-Router User Interface
 Created: 2021/04/26 19:57:43
-Last modified: 2021/06/03 21:19:40
+Last modified: 2021/09/23 22:02:18
+Flask server for Pi-Router User Interface
 '''
 
 from flask import (Flask, g, render_template, redirect, url_for, 
@@ -11,6 +11,7 @@ from passlib.hash import pbkdf2_sha256
 import subprocess
 import os
 import glob
+import re
 
 # Connect to user database
 from db import Database
@@ -20,6 +21,8 @@ app = Flask(__name__)
 app.secret_key = "super secret key"
 
 login_message = "Authentication required for this action"
+CONF_FILE = "/etc/hostapd/hostapd.conf"
+
 
 def get_db():
     db = getattr(g, '_database', None)
@@ -27,37 +30,28 @@ def get_db():
         db = Database(DATABASE_PATH)
     return db
 
+
 @app.teardown_appcontext
 def close_db(exception):
     db = getattr(g, '_database', None)
     if db is not None:
         db.close()
 
+
 @app.route("/", methods=["GET", "POST"])
 def home():
     if 'user' in session:
         name = session['user']['name']
-
-        # Get network settings
-        config_file = open("/etc/hostapd/hostapd.conf", "r")
-        credentials = config_file.readlines()
-        
-        # Get connected devices
-        devices = []
-        for device in os.popen('arp -a'): 
-            device = device.split()
-            device_name = device[0]
-            IP = device[1]
-            MAC = device[3]
-            interface = device[-1]
-            devices.append([device_name, IP, MAC, interface]) 
+        credentials = get_credentials()
+        devices = get_connected_devices()
         
         return render_template("index.html", name=name, 
                                credentials=credentials, 
                                devices=devices, images=update_images())
     else:
-        return render_template('login.html' ) 
+        return render_template('login.html') 
     
+
 @app.route('/create_user', methods=['GET', 'POST'])
 def create_user():
     if request.method == 'POST':
@@ -69,6 +63,7 @@ def create_user():
             get_db().create_user(name, username, encrypted_password)
             return redirect('/login')
     return render_template('create_user.html')
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -92,41 +87,32 @@ def login():
             message = "Missing username, please try again"
     return render_template('login.html', message=message)
 
+
 @app.route('/edit', methods=['GET', 'POST'])
 def edit():
     if 'user' in session:
-        filename = "/etc/hostapd/hostapd.conf"
-        config_file = open(filename, "r")
-        credentials = config_file.readlines()
-        current_ssid = credentials[2].strip("\n").split("ssid=")[1]
-        current_password = credentials[8].strip("\n").split("wpa_passphrase=")[1]
-
+        credentials = get_credentials()
         if request.method == "POST":
-            change = ''
             setting = request.form.get('setting')
             new_val = request.form.get('new_value')
-            # Update the SSID by calling subprocess
-            if setting == "ssid" and new_val != None:
-                subprocess.call(['sed', '-i', 's/ssid=%s/ssid=%s/' % (current_ssid, new_val),
-                    filename])
-                change = "Success! ssid was changed from {} to {}".format(current_ssid, new_val)
-            # Update the password by calling subprocess
-            elif setting == "password" and new_val != None:
-                subprocess.call(['sed', '-i', 's/wpa_passphrase=%s/wpa_passphrase=%s/' 
-                % (current_password, new_val), filename])
-                change = "Success! password was changed from {} to {}".format(current_password, new_val)
-                
-            # Display new settings
-            config_file = open(filename, "r")
-            credentials = config_file.readlines()
-    
-            if change:
-                return render_template('edit.html', credentials=credentials, change=change)
+            
+            # Get current values
+            if setting == "ssid":
+                current_val = credentials[1].strip("\n").split("=")[1]
+            elif setting == "password": 
+                current_val = credentials[7].strip("\n").split("wpa_passphrase=")[1]
 
+            # Update the desired setting and display changes
+            if new_val:
+                success_string = update_setting(setting, current_val, new_val)
+                credentials = get_credentials()
+                return render_template('edit.html', credentials=credentials, change=success_string)
+                
         return render_template('edit.html', credentials=credentials)
     else:
         return render_template('login.html', message=login_message) 
-    
+
+
 @app.route('/network_health', methods=['GET', 'POST'])
 def network_health():
     if 'user' in session:
@@ -134,24 +120,60 @@ def network_health():
     else:
         return render_template('login.html', message=login_message) 
 
+
 @app.route('/reboot', methods=['GET', 'POST'])
 def reboot():
     if 'user' in session:
         subprocess.call(['shutdown', '-r', 'now'])
     else:
         return render_template('login.html', message=login_message) 
-         
+
+
 @app.route('/logout')
 def logout():
     session.pop('user', None)
     return redirect('/login')
 
+
 def update_images():
     # Copy newest graphs from malware detection repo and get list of names
-    subprocess.call(['cp', '-r', '/home/pi/malware_detection/figures/classification/5s/.', 'static/img/malware_detection'])
+    subprocess.call('cp -r /home/pi/malware_detection/figures/classification/5s/. static/img/malware_detection', shell=True)
     images = glob.glob("static/img/malware_detection/*")
 
     return images
+
+
+def update_setting(setting, current_val, new_val):
+    # Update a network setting
+    rc = subprocess.call(['/usr/bin/sed', '-i', f's/{setting}={current_val}/{setting}={new_val}/', CONF_FILE])
+    if rc == 0:
+        return f"Success! Your {setting} was changed from {current_val} to {new_val}."
+    else:
+        return "Update process failed."
+    
+    return feedback
+
+
+def get_connected_devices():
+    # Get connected devices
+    devices = []
+    for device in os.popen('/usr/sbin/arp -a'): 
+        device = device.split()
+        device_name = device[0]
+        IP = re.sub('[()]', '', device[1])
+        MAC = device[3]
+        interface = device[-1]
+        devices.append([device_name, IP, MAC, interface]) 
+
+    return devices
+
+
+def get_credentials():
+    # Get network settings
+    config_file = open(CONF_FILE, "r")
+
+    return config_file.readlines()
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
